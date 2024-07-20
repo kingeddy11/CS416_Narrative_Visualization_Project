9a8828e6887f5635ae399b35ddf04d29842f16f4
Edward Wu <ewu23@students.claremontmckenna.edu>
2024-07-20 00:39:34 -0400

    checking chart outlook

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ async function scene_25_35() {
         svg.append("text")
             .attr("class", "x_axis_label")
             .attr("x", width / 2)
-            .attr("y", height + margin.bottom - 10)
+            .attr("y", height + margin.bottom - 20)
             .style("text-anchor", "middle")
             .text("Region");
 
@@ -109,7 +109,7 @@ async function scene_25_35() {
         svg.append("text")
             .attr("class", "y_axis_label")
             .attr("transform", "rotate(-90)")
-            .attr("x", -height / 2 + 10)
+            .attr("x", -height / 2 + 20)
             .attr("y", -margin.left + 20)
             .style("text-anchor", "middle")
             .text("Percentage of Population (in percent)");
