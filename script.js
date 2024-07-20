c27e5b090b8b12d5a9d42070f2708e2f0d4c70a0
Edward Wu <ewu23@students.claremontmckenna.edu>
2024-07-20 00:33:13 -0400

    checking chart look

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Setting margins
-const margin = { top: 30, right: 40, bottom: 70, left: 60 };
+const margin = { top: 30, right: 100, bottom: 70, left: 80 };
 const width = 800 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
@@ -39,7 +39,7 @@ async function scene_25_35() {
     // Legend for amount of education completed
     var legend = svg.append("g")
         .attr("class", "legend")
-        .attr("transform", `translate(${width - 100}, 0)`);
+        .attr("transform", `translate(${width + margin.right - 100}, 0)`);
 
     legend.selectAll("rect")
         .data(subgroups)
@@ -109,7 +109,7 @@ async function scene_25_35() {
         svg.append("text")
             .attr("class", "y_axis_label")
             .attr("transform", "rotate(-90)")
-            .attr("x", -height / 2 - 10)
+            .attr("x", -height / 2 + 10)
             .attr("y", -margin.left + 20)
             .style("text-anchor", "middle")
             .text("Percentage of Population (in percent)");
